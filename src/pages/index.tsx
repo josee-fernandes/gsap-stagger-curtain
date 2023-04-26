e8e2336fbf3875a427c54fc39b32a14c1eb9a9ed
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2023-04-26 15:13:32 -0300

    Opening stagger curtain animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,100 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Power3, gsap } from 'gsap'
+import { NextPage } from 'next'
+import { useCallback, useEffect } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const Home: NextPage = () => {
+  const animateText = useCallback(() => {
+    gsap.fromTo(
+      '.text',
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, duration: 0.8, ease: Power3.easeInOut }
+    )
+  }, [])
 
-export default function Home() {
+  const animateCurtain = useCallback(
+    (side: string) => {
+      const __class = side === 'left' ? '.curtain-left' : '.curtain-right'
+
+      const timeline = gsap.timeline()
+
+      timeline.to(__class, { display: 'block' })
+      timeline.to('.line', { display: 'none' })
+      timeline.fromTo(
+        __class,
+        { x: 0, display: 'none', stagger: 0.1 },
+        {
+          x: side === 'left' ? '-150%' : '150%',
+          display: 'block',
+          skewX: 30,
+          duration: 2,
+          stagger: 0.1,
+          delay: 2,
+          ease: Power3.easeInOut,
+        }
+      )
+
+      timeline.then(() => animateText())
+    },
+    [animateText]
+  )
+
+  const animateLine = useCallback(() => {
+    const timeline = gsap.timeline()
+
+    timeline.fromTo(
+      '.line',
+      { height: 0 },
+      { height: '50%', duration: 1.4, ease: Power3.easeInOut }
+    )
+    timeline.fromTo(
+      '.line',
+      { rotate: 0, translateX: '-50%', translateY: '-50%' },
+      {
+        rotate: 360,
+        translateX: '-50%',
+        translateY: '-50%',
+        duration: 1.4,
+        ease: Power3.easeInOut,
+      }
+    )
+    timeline.fromTo(
+      '.line',
+      { width: 4 },
+      { width: '100%', duration: 1.2, ease: Power3.easeInOut }
+    )
+    timeline.fromTo(
+      '.line',
+      { height: '50%' },
+      { height: '100%', duration: 1, ease: Power3.easeInOut }
+    )
+
+    timeline.then(() => {
+      animateCurtain('left')
+      animateCurtain('right')
+    })
+  }, [animateCurtain])
+
+  useEffect(() => {
+    animateLine()
+  }, [animateLine])
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="w-full h-full relative">
+      <div className="line fixed h-1/2 w-1 bg-zinc-50 left-1/2 top-1/2"></div>
+      <div className="curtain-left bg-zinc-50 w-1/2 h-screen fixed z-40 left-0 hidden"></div>
+      <div className="curtain-left bg-cyan-500 w-1/2 h-screen fixed z-30 left-0 hidden"></div>
+      <div className="curtain-left bg-fuchsia-500 w-1/2 h-screen fixed z-20 left-0 hidden"></div>
+      <div className="curtain-left bg-amber-500 w-1/2 h-screen fixed z-10 left-0 hidden"></div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <div className="curtain-right bg-zinc-50 w-1/2 h-screen fixed z-40 left-1/2 hidden"></div>
+      <div className="curtain-right bg-cyan-500 w-1/2 h-screen fixed z-30 left-1/2 hidden"></div>
+      <div className="curtain-right bg-fuchsia-500 w-1/2 h-screen fixed z-20 left-1/2 hidden"></div>
+      <div className="curtain-right bg-amber-500 w-1/2 h-screen fixed z-10 left-1/2 hidden"></div>
+      <p className="text text-center -skew-x-12 font-black text-7xl fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0">
+        🍿⚡🔥🎉
+      </p>
+    </div>
   )
 }
+
+export default Home
